77d8577795408c73bd8bd5516645527954470b5b
Son <son123@gmail.com>
2022-12-27 16:26:19 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,9 @@
+/* 
+Name : See Pao Yeong
+Matrix Number : S58079
+Description : This application will show the user a list of question for user to answer. A respond page will show up once the user choose his/her answer. Once all the questions in the question desk are completed by the user. The application will start all over again.
+*/
+
 import 'package:flutter/material.dart';
 import 'package:s58079_individual_assignment_2/question.dart';
 
